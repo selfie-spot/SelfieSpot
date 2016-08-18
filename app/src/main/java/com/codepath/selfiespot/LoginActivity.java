5d6eb420eefacd0456cd1409b911d63f3b7d05bb
Nidhi Saini <NidhiSaini@nidhis-mbp.gateway.pace.com>
2016-08-18 11:02:34 -0700

    second commit

--- app/src/main/java/com/codepath/selfiespot/LoginActivity.java
+++ app/src/main/java/com/codepath/selfiespot/LoginActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class LogActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
