3f87698d1988ab66e3d007677d48a5020bb03c26
Nidhi Saini <NidhiSaini@nidhis-mbp.gateway.pace.com>
2016-08-18 10:52:05 -0700

    first version of login button completed

---
+++ app/src/main/java/com/codepath/selfiespot/LogActivity.java
@@ -0,0 +1,21 @@
+package com.codepath.selfiespot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        decorView.setSystemUiVisibility(uiOptions);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
