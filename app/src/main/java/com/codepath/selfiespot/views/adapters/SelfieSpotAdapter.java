a5ec2df2cadad35e8d5485e5d563e94818d5183f
sharathp <chandra.ps@gmail.com>
2016-08-29 00:14:58 -0400

    Modified selfie spot item to display more information.

--- app/src/main/java/com/codepath/selfiespot/views/adapters/SelfieSpotAdapter.java
+++ app/src/main/java/com/codepath/selfiespot/views/adapters/SelfieSpotAdapter.java
@@ -2,15 +2,18 @@ package com.codepath.selfiespot.views.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.codepath.selfiespot.R;
 import com.codepath.selfiespot.models.SelfieSpot;
+import com.codepath.selfiespot.util.ViewUtils;
+import com.parse.ParseException;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -19,6 +22,8 @@ import butterknife.ButterKnife;
 
 
 public class SelfieSpotAdapter extends RecyclerView.Adapter<SelfieSpotAdapter.ViewHolder> {
+    private static final String TAG = SelfieSpotAdapter.class.getSimpleName();
+
     private List<SelfieSpot> mSelfieSpots;
 
     public SelfieSpotAdapter(final List<SelfieSpot> mSelfieSpots) {
@@ -53,17 +58,17 @@ public class SelfieSpotAdapter extends RecyclerView.Adapter<SelfieSpotAdapter.Vi
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        @BindView(R.id.ivSelfieSpotImage)
-        ImageView ivSelfieSpotImage;
+        @BindView(R.id.ivImage)
+        ImageView mImageView;
 
-        @BindView(R.id.tvSelfieSpotName)
-        TextView tvSelfieSpotName;
+        @BindView(R.id.tvName)
+        TextView mNameTextView;
 
-        @BindView(R.id.tvSelfieSpotDesc)
-        TextView tvSelfieSpotDesc;
+        @BindView(R.id.tvLikes)
+        TextView mLikesTextView;
 
-        @BindView(R.id.rbSelfieSpotRatingBar)
-        RatingBar rbSelfieSpotRatingBar;
+        @BindView(R.id.tvAuthor)
+        TextView mAuthorTextView;
 
         public ViewHolder(final View itemView) {
             super(itemView);
@@ -71,8 +76,23 @@ public class SelfieSpotAdapter extends RecyclerView.Adapter<SelfieSpotAdapter.Vi
         }
 
         public void bind(final SelfieSpot selfieSpot) {
-            tvSelfieSpotName.setText(selfieSpot.getName());
-            tvSelfieSpotDesc.setText(selfieSpot.getDescription());
+            mNameTextView.setText(selfieSpot.getName());
+            mLikesTextView.setText(ViewUtils.getSpannedText(itemView.getContext(),
+                    itemView.getContext().getResources().getString(R.string.text_likes), selfieSpot.getLikesCount()));
+            try {
+                mAuthorTextView.setText(String.format(itemView.getContext().getResources().getString(R.string.text_by),
+                        selfieSpot.getUser().fetchIfNeeded().getUsername()));
+            } catch (final ParseException e) {
+                Log.e(TAG, "Unable to retrieve username", e);
+            }
+
+            Picasso.with(itemView.getContext())
+                    .load(selfieSpot.getMediaFile().getUrl())
+                    .fit()
+                    .centerInside()
+                    .placeholder(R.drawable.ic_progress_indeterminate)
+                    .error(R.drawable.ic_error)
+                    .into(mImageView);
         }
     }
 }
