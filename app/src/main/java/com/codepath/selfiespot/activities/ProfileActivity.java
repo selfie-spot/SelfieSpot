e59536cea677479ce381904a545448a5371c2a1d
sharathp <chandra.ps@gmail.com>
2016-08-29 23:16:23 -0400

    Removed pagerslidingtabstrip, using android default TabLayout.

--- app/src/main/java/com/codepath/selfiespot/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/selfiespot/activities/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.codepath.selfiespot.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,10 +11,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import com.astuetz.PagerSlidingTabStrip;
 import com.codepath.selfiespot.R;
-import com.codepath.selfiespot.fragments.MySelfiesFragment;
 import com.codepath.selfiespot.fragments.BookmarkedSelfiesFragment;
+import com.codepath.selfiespot.fragments.MySelfiesFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,10 +21,10 @@ import butterknife.ButterKnife;
 public class ProfileActivity extends AppCompatActivity {
 
     @BindView(R.id.viewpager)
-    ViewPager viewPager;
+    ViewPager mViewPager;
 
-    @BindView(R.id.tabs)
-    PagerSlidingTabStrip tabsStrip;
+    @BindView(R.id.tl_profile)
+    TabLayout mTabLayout;
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
@@ -44,19 +44,17 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
-        viewPager.setAdapter(new SelfieSpotsPagerAdapter(getSupportFragmentManager()));
-        tabsStrip.setViewPager(viewPager);
-
+        mViewPager.setAdapter(new SelfieSpotsPagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
     }
-
 
     class SelfieSpotsPagerAdapter extends FragmentPagerAdapter {
         private final int PAGE_COUNT = 2;
         private final int POSITION_MY_SELFIES = 0;
         private final int POSITION_BOOKMARK_SELFIES = 1;
-        private String tabTitles[] = new String[] {"My Selfies", "Saved Selfies"};
+        private String mTabTitles[] = new String[] {"My Selfies", "Bookmarks"};
 
-        public SelfieSpotsPagerAdapter(FragmentManager fm) {
+        public SelfieSpotsPagerAdapter(final FragmentManager fm) {
             super(fm);
         }
 
@@ -75,7 +73,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return tabTitles[position];
+            return mTabTitles[position];
         }
 
         @Override
