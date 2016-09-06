fa7faa09eefd2216e608b061a97f5113c6f683e1
sharathp <chandra.ps@gmail.com>
2016-09-06 19:54:45 -0400

    modified map to show abbreviated likes count.

--- app/src/main/java/com/codepath/selfiespot/util/NumberUtils.java
+++ app/src/main/java/com/codepath/selfiespot/util/NumberUtils.java
@@ -3,10 +3,12 @@ package com.codepath.selfiespot.util;
 public class NumberUtils {
 
     public static String withSuffix(final int count) {
-        if (count < 1000) return "" + count;
-        int exp = (int) (Math.log(count) / Math.log(1000));
-        return String.format("%.1f %c",
+        if (count < 1000) {
+            return "" + count;
+        }
+        final int exp = (int) (Math.log(count) / Math.log(1000));
+        return String.format("%.1f%c",
                 count / Math.pow(1000, exp),
-                "kMGTPE".charAt(exp-1));
+                "KMGTPE".charAt(exp - 1));
     }
 }
